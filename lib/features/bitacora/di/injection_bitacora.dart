371b2bd3d39341d9a843e684288afa7ffb99be41
Deyvit Sanchez <deyvit200426@gmail.com>
2026-07-20 22:22:21 -0600

    feat: rediseña bitacora con parcela automatica, selector de fecha y estado de mazorca

--- lib/features/bitacora/di/injection_bitacora.dart
+++ lib/features/bitacora/di/injection_bitacora.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 
 import '../../../core/session/session_service.dart';
+import '../../parcela/domain/parcela_repository.dart';
 import '../data/repositories/bitacora_repository_impl.dart';
 import '../domain/bitacora_repository.dart';
 import '../presentation/provider/bitacora_provider.dart';
@@ -10,6 +11,9 @@ void initBitacoraDependencies(GetIt getIt) {
         () => BitacoraRepositoryImpl(session: getIt<SessionService>()),
   );
   getIt.registerFactory<BitacoraProvider>(
-        () => BitacoraProvider(repository: getIt<BitacoraRepository>()),
+        () => BitacoraProvider(
+      repository: getIt<BitacoraRepository>(),
+      parcelaRepository: getIt<ParcelaRepository>(),
+    ),
   );
 }
--- lib/features/bitacora/domain/entities/bitacora.dart
+++ lib/features/bitacora/domain/entities/bitacora.dart
@@ -1,66 +1,110 @@
+enum EstadoMazorca {
+  sinSintomas,
+  manchasLeves,
+  manchasExtendidas,
+  pudricionVisible;
+
+  String get valorBackend => switch (this) {
+    EstadoMazorca.sinSintomas => 'sin_sintomas',
+    EstadoMazorca.manchasLeves => 'manchas_leves',
+    EstadoMazorca.manchasExtendidas => 'manchas_extendidas',
+    EstadoMazorca.pudricionVisible => 'pudricion_visible',
+  };
+
+  String get label => switch (this) {
+    EstadoMazorca.sinSintomas => 'Sin síntomas',
+    EstadoMazorca.manchasLeves => 'Manchas leves',
+    EstadoMazorca.manchasExtendidas => 'Manchas extendidas',
+    EstadoMazorca.pudricionVisible => 'Pudrición visible',
+  };
+
+  static EstadoMazorca? fromBackend(String? valor) {
+    return switch (valor) {
+      'sin_sintomas' => EstadoMazorca.sinSintomas,
+      'manchas_leves' => EstadoMazorca.manchasLeves,
+      'manchas_extendidas' => EstadoMazorca.manchasExtendidas,
+      'pudricion_visible' => EstadoMazorca.pudricionVisible,
+      _ => null,
+    };
+  }
+}
+
 class Bitacora {
-  final String?  id;
-  final String   destino;
-  final double   destinoLat;
-  final double   destinoLng;
-  final String   texto;
-  final double?  temperatura;
-  final double?  humedad;
-  final double?  precipitacion;
-  final bool     sincronizada;
-  final DateTime creadaEn;
+  final String?         id;
+  final String          destino;
+  final double           destinoLat;
+  final double           destinoLng;
+  final String?          texto;
+  final double?          temperatura;
+  final double?          humedad;
+  final double?          precipitacion;
+  final DateTime?        fechaObservacion;
+  final EstadoMazorca?   estadoMazorca;
+  final bool             sincronizada;
+  final DateTime         creadaEn;
 
   const Bitacora({
     this.id,
     required this.destino,
     required this.destinoLat,
     required this.destinoLng,
-    required this.texto,
+    this.texto,
     this.temperatura,
     this.humedad,
     this.precipitacion,
+    this.fechaObservacion,
+    this.estadoMazorca,
     this.sincronizada = false,
     required this.creadaEn,
   });
 
   Map<String, dynamic> toJson() => {
-    'destino':       destino,
-    'destinoLat':    destinoLat,
-    'destinoLng':    destinoLng,
-    'texto':         texto,
-    'temperatura':   temperatura,
-    'humedad':       humedad,
-    'precipitacion': precipitacion,
+    'destino':          destino,
+    'destinoLat':       destinoLat,
+    'destinoLng':       destinoLng,
+    'texto':            texto,
+    'temperatura':      temperatura,
+    'humedad':          humedad,
+    'precipitacion':    precipitacion,
+    'fechaObservacion': fechaObservacion?.toIso8601String(),
+    'estadoMazorca':    estadoMazorca?.valorBackend,
   };
 
   factory Bitacora.fromJson(Map<String, dynamic> j) => Bitacora(
-    id:            j['id'] as String?,
-    destino:       j['destino'] as String,
-    destinoLat:    (j['destinoLat'] as num).toDouble(),
-    destinoLng:    (j['destinoLng'] as num).toDouble(),
-    texto:         j['texto'] as String,
-    temperatura:   (j['temperatura'] as num?)?.toDouble(),
-    humedad:       (j['humedad'] as num?)?.toDouble(),
-    precipitacion: (j['precipitacion'] as num?)?.toDouble(),
-    sincronizada:  j['sincronizada'] as bool? ?? true,
-    creadaEn:      DateTime.parse(j['creadaEn'] as String? ?? j['creada_en'] as String),
+    id:               j['id'] as String?,
+    destino:          j['destino'] as String,
+    destinoLat:       (j['destinoLat'] as num).toDouble(),
+    destinoLng:       (j['destinoLng'] as num).toDouble(),
+    texto:            j['texto'] as String?,
+    temperatura:      (j['temperatura'] as num?)?.toDouble(),
+    humedad:          (j['humedad'] as num?)?.toDouble(),
+    precipitacion:    (j['precipitacion'] as num?)?.toDouble(),
+    fechaObservacion: j['fechaObservacion'] != null
+        ? DateTime.parse(j['fechaObservacion'] as String)
+        : null,
+    estadoMazorca:    EstadoMazorca.fromBackend(j['estadoMazorca'] as String?),
+    sincronizada:     j['sincronizada'] as bool? ?? true,
+    creadaEn:         DateTime.parse(j['creadaEn'] as String? ?? j['creada_en'] as String),
   );
 
-  // Para guardar local (cifrado) antes de sincronizar
   Map<String, dynamic> toLocalJson() => {
     ...toJson(),
     'creadaEn': creadaEn.toIso8601String(),
   };
 
   factory Bitacora.fromLocalJson(Map<String, dynamic> j) => Bitacora(
-    destino:       j['destino'] as String,
-    destinoLat:    (j['destinoLat'] as num).toDouble(),
-    destinoLng:    (j['destinoLng'] as num).toDouble(),
-    texto:         j['texto'] as String,
-    temperatura:   (j['temperatura'] as num?)?.toDouble(),
-    humedad:       (j['humedad'] as num?)?.toDouble(),
-    precipitacion: (j['precipitacion'] as num?)?.toDouble(),
-    sincronizada:  false,
-    creadaEn:      DateTime.parse(j['creadaEn'] as String),
+    destino:          j['destino'] as String,
+    destinoLat:       (j['destinoLat'] as num).toDouble(),
+    destinoLng:       (j['destinoLng'] as num).toDouble(),
+    texto:            j['texto'] as String?,
+    temperatura:      (j['temperatura'] as num?)?.toDouble(),
+    humedad:          (j['humedad'] as num?)?.toDouble(),
+    precipitacion:    (j['precipitacion'] as num?)?.toDouble(),
+    fechaObservacion: j['fechaObservacion'] != null
+        ? DateTime.parse(j['fechaObservacion'] as String)
+        : null,
+    estadoMazorca:    EstadoMazorca.fromBackend(j['estadoMazorca'] as String?),
+    sincronizada:     false,
+    creadaEn:         DateTime.parse(j['creadaEn'] as String),
   );
 }
